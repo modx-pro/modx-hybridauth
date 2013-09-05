4c553a6ecacb791a1c4df58af193ddcb67b9a2ea
bezumkin <bezumkin@yandex.ru>
2013-09-05 18:15:27 +0700

    Automatic logout of users that was been blocked by administrator

--- core/components/hybridauth/elements/plugins/plugin.hybridauth.php
+++ core/components/hybridauth/elements/plugins/plugin.hybridauth.php
@@ -2,6 +2,13 @@
 switch ($modx->event->name) {
 
 	case 'OnHandleRequest':
+		if ($modx->user->isAuthenticated()) {
+			if (!$modx->user->active || $modx->user->Profile->blocked) {
+				$modx->runProcessor('security/logout');
+				$modx->sendRedirect($modx->makeUrl($modx->getOption('site_start'),'','','full'));
+			}
+		}
+
 		if (!empty($_REQUEST['hauth_action']) || !empty($_REQUEST['hauth_start']) || !empty($_REQUEST['hauth_done'])) {
 			
 			if (!$modx->loadClass('hybridauth', MODX_CORE_PATH . 'components/hybridauth/model/hybridauth/', false, true)) {return;}
