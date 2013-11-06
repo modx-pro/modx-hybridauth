8240173236446f7f830f6cc59580f7d0b3262573
Bezumkin <bezumkin@yandex.ru>
2013-11-06 09:27:35 +0700

    Improved package uninstall and reinstall Version 0.8.5-pl

--- _build/build.config.php
+++ _build/build.config.php
@@ -3,7 +3,7 @@
 define('PKG_NAME','HybridAuth');
 define('PKG_NAME_LOWER',strtolower(PKG_NAME));
 
-define('PKG_VERSION','0.8.4');
+define('PKG_VERSION','0.8.5');
 define('PKG_RELEASE','pl');
 define('PKG_AUTO_INSTALL', true);
 
--- _build/resolvers/resolve.tables.php
+++ _build/resolvers/resolve.tables.php
@@ -11,19 +11,22 @@ if ($object->xpdo) {
 
 	switch ($options[xPDOTransport::PACKAGE_ACTION]) {
 		case xPDOTransport::ACTION_INSTALL:
+		case xPDOTransport::ACTION_UPGRADE:
 			$modelPath = $modx->getOption('hybridauth.core_path',null,$modx->getOption('core_path').'components/hybridauth/').'model/';
 			$modx->addPackage('hybridauth',$modelPath);
 
 			$manager = $modx->getManager();
 			$manager->createObjectContainer('haUserService');
 			$modx->addExtensionPackage('hybridauth', '[[++core_path]]components/hybridauth/model/');
-			break;
 
-		case xPDOTransport::ACTION_UPGRADE:
+			$modx->exec("UPDATE {$modx->getTableName('modUser')} SET `class_key` = 'haUser' WHERE `class_key` = 'modUser' AND `id` IN (
+					SELECT DISTINCT(`internalKey`) as `id` FROM {$modx->getTableName('haUserService')}
+			);");
+
 			break;
 
 		case xPDOTransport::ACTION_UNINSTALL:
-			$stmt = $modx->exec("UPDATE {$modx->getTableName('modUser')} SET `class_key` = 'modUser' WHERE `class_key` = 'haUser';");
+			$modx->exec("UPDATE {$modx->getTableName('modUser')} SET `class_key` = 'modUser' WHERE `class_key` = 'haUser';");
 			$modx->removeExtensionPackage('hybridauth');
 			break;
 	}
